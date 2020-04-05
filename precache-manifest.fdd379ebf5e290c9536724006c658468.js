2f627ad12b30730c5438fcbf4f3335b0e08777cb
Alan Kendy Higa <alankhiga@gmail.com>
2020-04-05 10:17:50 -0300

    Updates

---
+++ precache-manifest.fdd379ebf5e290c9536724006c658468.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27a3088dff932a6ff3bdbc77fb3e58f",
+    "url": "/gh-page/index.html"
+  },
+  {
+    "revision": "3ee8613654bee45a7a3b",
+    "url": "/gh-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85f9b17398735efa8c1a",
+    "url": "/gh-page/static/js/2.0ca86ec9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-page/static/js/2.0ca86ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee8613654bee45a7a3b",
+    "url": "/gh-page/static/js/main.a5c6ba38.chunk.js"
+  },
+  {
+    "revision": "2ba1de91075f0857a530",
+    "url": "/gh-page/static/js/runtime-main.4e546477.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
