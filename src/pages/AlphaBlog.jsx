bed97dcd37369699e37294287072e2673cae8a9b
Alan Kendy Higa <alankhiga@gmail.com>
2020-04-09 23:37:59 -0300

    adding react-images carousel

--- src/pages/AlphaBlog.jsx
+++ src/pages/AlphaBlog.jsx
@@ -5,6 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 import '../List.css';
 
+import Carousel from 'react-images';
+import alphaImage1 from './alpha-blog1.png'
+import alphaImage2 from './alpha-blog2.png'
+
+const images = [{ source: alphaImage1}, {source: alphaImage2 }];
+
 function AlphaBlog() {
   return (
     <div>
@@ -16,9 +22,10 @@ function AlphaBlog() {
           Udemy: <a href='https://www.udemy.com/course/the-complete-ruby-on-rails-developer-course/' target='_blank'>
             The Complete Ruby on Rails Developer Course</a>.
         </span>
-
         <h4><Link to="/">Voltar</Link></h4>
       </div>
+      <h4>Screenshoots:</h4>
+      <Carousel views={images} />;
     </div>
   );
 }
