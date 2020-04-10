41c5f67742d0e21459b9638ac774d62c23cdc8a9
Alan Kendy Higa <alankhiga@gmail.com>
2020-04-09 22:32:08 -0300

    changing repo link

--- src/pages/AlphaBlog.jsx
+++ src/pages/AlphaBlog.jsx
@@ -9,7 +9,8 @@ function AlphaBlog() {
   return (
     <div>
       <div className='App'>
-        <h1><a href='https://github.com/kendyhiga/alpha-blog' target='_blank'>Alpha Blog</a></h1>
+        <h1>Alpha Blog</h1>
+        <p><a href='https://github.com/kendyhiga/alpha-blog' target='_blank'>[Repositório]</a></p>
         <span>Este projeto em Ruby on Rails consiste
           num blog, que foi criado a partir do curso da
           Udemy: <a href='https://www.udemy.com/course/the-complete-ruby-on-rails-developer-course/' target='_blank'>
