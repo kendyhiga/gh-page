6f6804a56b9d8db9a4cafc694fc70a575df247de
Alan Kendy Higa <alankhiga@gmail.com>
2020-04-15 21:59:28 -0300

    adding filter options

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import Card from './Card'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 
-function HomePage() {
-  return (
-    <div className='container'>
-      <div className="jumbotron jumbotron-fluid jumbotron-custom">
-        <h1 className="display-4">Meus repositórios do <a href='https://github.com/kendyhiga/' target='_blank' rel='noopener noreferrer'>GitHub</a></h1>
-        <p className="lead">Este SPA foi criado em React com objetivo resumir alguns dos meus projetos.</p>
-      </div>
-      <div className='row'>
-        <div className="card-deck">
-          <Card link='/alpha-blog' name='AlphaBlog'/>
-          <Card link='/calculator' name='Calculadora'/>
-          <Card link='/cookbook' name='Cookbook'/>
+export default class HomePage extends Component {
+  constructor() {
+    super();
+    const cards = [
+      { language: 'ruby', link: '/alpha-blog', name: 'AlphaBlog' },
+      { language: 'react', link: '/calculator', name: 'Calculadora' },
+      { language: 'ruby', link: '/cookbook', name: 'Cookbook' }
+    ]
+    this.state = {
+      cards: cards,
+      filtered_cards: cards
+    };
+  }
+
+  setFilter = (filter) => {
+    if(filter === 'all') {
+      return this.setState(state => ({
+        filtered_cards: this.state.cards
+      }));
+    }
+    const cards = this.state.cards.filter(card => card.language === filter)
+      this.setState(state => ({
+      filtered_cards: cards
+    }));
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <div className="jumbotron jumbotron-fluid jumbotron-custom">
+          <h1 className="display-4">Meus repositórios do <a href='https://github.com/kendyhiga/' target='_blank' rel='noopener noreferrer'>GitHub</a></h1>
+          <p className="lead">Este SPA foi criado em React com objetivo resumir alguns dos meus projetos.</p>
+        </div>
+          <div className="btn-group d-flex" role="group">
+            <button className="btn btn-primary" onClick={() => this.setFilter('all')}>Tudo</button>
+            <button className="btn btn-primary" onClick={() => this.setFilter('ruby')}>Ruby</button>
+            <button className="btn btn-primary" onClick={() => this.setFilter('react')}>React</button>
+        </div>
+        <br></br>
+        <div className='row'>
+          <div className="card-deck">
+            {
+              this.state.filtered_cards.map((card, index) =>
+                <Card key={index} link={card.link} name={card.name}/>
+              )
+            }
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default HomePage;
