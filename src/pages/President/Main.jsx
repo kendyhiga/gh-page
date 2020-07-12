5886711f7e4d426b9818cf8b879806320c93f513
Alan Kendy Higa <alankhiga@gmail.com>
2020-07-12 17:28:39 -0300

    firebase initial config

--- src/pages/President/Main.jsx
+++ src/pages/President/Main.jsx
@@ -1,22 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as firebase from 'firebase'
 
 import JumbotronSmall from '../JumbotronSmall'
 
-function Challenge() {
-  return (
-    <div>
-      <div className='container'>
-      <JumbotronSmall name={'Presidente'}/>
-        <h3>Choosing numbers between 1 and 10, whoever gets to 100 first, wins.</h3>
-        <h3>Would you like to go first?</h3>
-        <Link to='/challenge_me'><h4>Yes</h4></Link>
-        <Link to='/challenge_pc'><h4>No</h4></Link>
-        <br></br>
-        <h4><Link to='/'>Voltar</Link></h4>
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "gh-page.firebaseapp.com",
+  databaseURL: "https://gh-page.firebaseio.com",
+  projectId: "gh-page",
+  storageBucket: "gh-page.appspot.com",
+  messagingSenderId: "825478090892",
+  appId: "1:825478090892:web:043d98eb1d7c04d19c6ba9",
+  measurementId: "G-SRFR5S31XB"
+};
+
+firebase.initializeApp(config)
+
+class President extends Component {
+  constructor() {
+    super();
+    this.state = {
+      speed: 10
+    };
+  }
+
+  componentDidMount(){
+    const rootRef = firebase.database().ref();
+    const speedRef = rootRef.child('speed');
+    speedRef.on('value', snap => {
+      this.setState({
+        speed: snap.val()
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='container'>
+          <JumbotronSmall name={'Presidente'}/>
+          <h1>Finally {this.state.speed}</h1>
+          <br></br>
+          <h4><Link to='/'>Voltar</Link></h4>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default Challenge;
+export default President;
