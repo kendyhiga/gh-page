47e9be468ea3d20d3fff602dd962d2c1fc8f7233
Alan Kendy Higa <alankhiga@gmail.com>
2020-07-19 19:12:04 -0300

    adding svg cards

--- src/pages/President/Game.jsx
+++ src/pages/President/Game.jsx
@@ -57,23 +57,26 @@ import { ReactComponent as S11 } from './SVG-cards/11-S.svg';
 import { ReactComponent as S12 } from './SVG-cards/12-S.svg';
 import { ReactComponent as S13 } from './SVG-cards/13-S.svg';
 
+import { ReactComponent as Joker } from './SVG-cards/JOKER.svg';
+
+
 
 class Game extends Component {
   constructor() {
     super();
-    const DECK = ["01-C", "01-D", "01-H", "01-S",
-              "02-C", "02-D", "02-H", "02-S",
-              "03-C", "03-D", "03-H", "03-S",
-              "04-C", "04-D", "04-H", "04-S",
-              "05-C", "05-D", "05-H", "05-S",
-              "06-C", "06-D", "06-H", "06-S",
-              "07-C", "07-D", "07-H", "07-S",
-              "08-C", "08-D", "08-H", "08-S",
-              "09-C", "09-D", "09-H", "09-S",
-              "10-C", "10-D", "10-H", "10-S",
-              "11-C", "11-D", "11-H", "11-S",
-              "12-C", "12-D", "12-H", "12-S",
-              "13-C", "13-D", "13-H", "13-S"]
+    const DECK = ["C01", "D01", "H01", "S01",
+                  "C02", "D02", "H02", "S02",
+                  "C03", "D03", "H03", "S03",
+                  "C04", "D04", "H04", "S04",
+                  "C05", "D05", "H05", "S05",
+                  "C06", "D06", "H06", "S06",
+                  "C07", "D07", "H07", "S07",
+                  "C08", "D08", "H08", "S08",
+                  "C09", "D09", "H09", "S09",
+                  "C10", "D10", "H10", "S10",
+                  "C11", "D11", "H11", "S11",
+                  "C12", "D12", "H12", "S12",
+                  "C13", "D13", "H13", "S13"]
     var shuffledDECK, player1Hand, player2Hand
     shuffledDECK = this.shuffle(DECK)
     player1Hand = shuffledDECK.slice(0, 26)
@@ -111,10 +114,71 @@ class Game extends Component {
     return (
       <div className='container'>
         <C01></C01>
+        <C02></C02>
+        <C03></C03>
+        <C04></C04>
+        <C05></C05>
+        <C06></C06>
+        <C07></C07>
+        <C08></C08>
+        <C09></C09>
+        <C10></C10>
+        <C11></C11>
+        <C12></C12>
+        <C13></C13>
+
+        <D01></D01>
+        <D02></D02>
+        <D03></D03>
+        <D04></D04>
+        <D05></D05>
+        <D06></D06>
+        <D07></D07>
+        <D08></D08>
+        <D09></D09>
+        <D10></D10>
+        <D11></D11>
+        <D12></D12>
+        <D13></D13>
+
+        <H01></H01>
+        <H02></H02>
+        <H03></H03>
+        <H04></H04>
+        <H05></H05>
+        <H06></H06>
+        <H07></H07>
+        <H08></H08>
+        <H09></H09>
+        <H10></H10>
+        <H11></H11>
+        <H12></H12>
+        <H13></H13>
+
+        <S01></S01>
+        <S02></S02>
+        <S03></S03>
+        <S04></S04>
+        <S05></S05>
+        <S06></S06>
+        <S07></S07>
+        <S08></S08>
+        <S09></S09>
+        <S10></S10>
+        <S11></S11>
+        <S12></S12>
+        <S13></S13>
+
+        <Joker></Joker>
+
         <h1>GAME</h1>
         <h2>shuffled deck: {this.state.shuffledDECK.join(', ')}</h2>
         <br></br>
         <h2>player 1 hand: {this.state.player1Hand.join(', ')}</h2>
+        <h2>player 1 hand: </h2>
+        {this.state.player1Hand.map((card, index) =>
+          eval(`1 + 1`)
+        )}
         <div className='btn btn-dark' onClick={() => sortHand(1, this.state.player1Hand)}>
           Ordenar mão
         </div>
