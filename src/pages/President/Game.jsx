0d97b3a3b574a08805a83160f6081604859ba61c
Alan Kendy Higa <alankhiga@gmail.com>
2020-07-19 23:22:16 -0300

    sortable svgs

--- src/pages/President/Game.jsx
+++ src/pages/President/Game.jsx
@@ -1,83 +1,84 @@
 import React, { Component } from 'react';
 import firebase from '../../firebaseConfig';
-import { render } from 'react-dom';
-import { ReactComponent as C01 } from './SVG-cards/01-C.svg';
-import { ReactComponent as C02 } from './SVG-cards/02-C.svg';
-import { ReactComponent as C03 } from './SVG-cards/03-C.svg';
-import { ReactComponent as C04 } from './SVG-cards/04-C.svg';
-import { ReactComponent as C05 } from './SVG-cards/05-C.svg';
-import { ReactComponent as C06 } from './SVG-cards/06-C.svg';
-import { ReactComponent as C07 } from './SVG-cards/07-C.svg';
-import { ReactComponent as C08 } from './SVG-cards/08-C.svg';
-import { ReactComponent as C09 } from './SVG-cards/09-C.svg';
-import { ReactComponent as C10 } from './SVG-cards/10-C.svg';
-import { ReactComponent as C11 } from './SVG-cards/11-C.svg';
-import { ReactComponent as C12 } from './SVG-cards/12-C.svg';
-import { ReactComponent as C13 } from './SVG-cards/13-C.svg';
+import { ReactSVG } from 'react-svg'
 
-import { ReactComponent as D01 } from './SVG-cards/01-D.svg';
-import { ReactComponent as D02 } from './SVG-cards/02-D.svg';
-import { ReactComponent as D03 } from './SVG-cards/03-D.svg';
-import { ReactComponent as D04 } from './SVG-cards/04-D.svg';
-import { ReactComponent as D05 } from './SVG-cards/05-D.svg';
-import { ReactComponent as D06 } from './SVG-cards/06-D.svg';
-import { ReactComponent as D07 } from './SVG-cards/07-D.svg';
-import { ReactComponent as D08 } from './SVG-cards/08-D.svg';
-import { ReactComponent as D09 } from './SVG-cards/09-D.svg';
-import { ReactComponent as D10 } from './SVG-cards/10-D.svg';
-import { ReactComponent as D11 } from './SVG-cards/11-D.svg';
-import { ReactComponent as D12 } from './SVG-cards/12-D.svg';
-import { ReactComponent as D13 } from './SVG-cards/13-D.svg';
+import CARD_01_C from './SVG-cards/C01.svg';
+import CARD_02_C from './SVG-cards/C02.svg';
+import CARD_03_C from './SVG-cards/C03.svg';
+import CARD_04_C from './SVG-cards/C04.svg';
+import CARD_05_C from './SVG-cards/C05.svg';
+import CARD_06_C from './SVG-cards/C06.svg';
+import CARD_07_C from './SVG-cards/C07.svg';
+import CARD_08_C from './SVG-cards/C08.svg';
+import CARD_09_C from './SVG-cards/C09.svg';
+import CARD_10_C from './SVG-cards/C10.svg';
+import CARD_11_C from './SVG-cards/C11.svg';
+import CARD_12_C from './SVG-cards/C12.svg';
+import CARD_13_C from './SVG-cards/C13.svg';
 
-import { ReactComponent as H01 } from './SVG-cards/01-H.svg';
-import { ReactComponent as H02 } from './SVG-cards/02-H.svg';
-import { ReactComponent as H03 } from './SVG-cards/03-H.svg';
-import { ReactComponent as H04 } from './SVG-cards/04-H.svg';
-import { ReactComponent as H05 } from './SVG-cards/05-H.svg';
-import { ReactComponent as H06 } from './SVG-cards/06-H.svg';
-import { ReactComponent as H07 } from './SVG-cards/07-H.svg';
-import { ReactComponent as H08 } from './SVG-cards/08-H.svg';
-import { ReactComponent as H09 } from './SVG-cards/09-H.svg';
-import { ReactComponent as H10 } from './SVG-cards/10-H.svg';
-import { ReactComponent as H11 } from './SVG-cards/11-H.svg';
-import { ReactComponent as H12 } from './SVG-cards/12-H.svg';
-import { ReactComponent as H13 } from './SVG-cards/13-H.svg';
+import CARD_01_D from './SVG-cards/D01.svg';
+import CARD_02_D from './SVG-cards/D02.svg';
+import CARD_03_D from './SVG-cards/D03.svg';
+import CARD_04_D from './SVG-cards/D04.svg';
+import CARD_05_D from './SVG-cards/D05.svg';
+import CARD_06_D from './SVG-cards/D06.svg';
+import CARD_07_D from './SVG-cards/D07.svg';
+import CARD_08_D from './SVG-cards/D08.svg';
+import CARD_09_D from './SVG-cards/D09.svg';
+import CARD_10_D from './SVG-cards/D10.svg';
+import CARD_11_D from './SVG-cards/D11.svg';
+import CARD_12_D from './SVG-cards/D12.svg';
+import CARD_13_D from './SVG-cards/D13.svg';
 
-import { ReactComponent as S01 } from './SVG-cards/01-S.svg';
-import { ReactComponent as S02 } from './SVG-cards/02-S.svg';
-import { ReactComponent as S03 } from './SVG-cards/03-S.svg';
-import { ReactComponent as S04 } from './SVG-cards/04-S.svg';
-import { ReactComponent as S05 } from './SVG-cards/05-S.svg';
-import { ReactComponent as S06 } from './SVG-cards/06-S.svg';
-import { ReactComponent as S07 } from './SVG-cards/07-S.svg';
-import { ReactComponent as S08 } from './SVG-cards/08-S.svg';
-import { ReactComponent as S09 } from './SVG-cards/09-S.svg';
-import { ReactComponent as S10 } from './SVG-cards/10-S.svg';
-import { ReactComponent as S11 } from './SVG-cards/11-S.svg';
-import { ReactComponent as S12 } from './SVG-cards/12-S.svg';
-import { ReactComponent as S13 } from './SVG-cards/13-S.svg';
+import CARD_01_H from './SVG-cards/H01.svg';
+import CARD_02_H from './SVG-cards/H02.svg';
+import CARD_03_H from './SVG-cards/H03.svg';
+import CARD_04_H from './SVG-cards/H04.svg';
+import CARD_05_H from './SVG-cards/H05.svg';
+import CARD_06_H from './SVG-cards/H06.svg';
+import CARD_07_H from './SVG-cards/H07.svg';
+import CARD_08_H from './SVG-cards/H08.svg';
+import CARD_09_H from './SVG-cards/H09.svg';
+import CARD_10_H from './SVG-cards/H10.svg';
+import CARD_11_H from './SVG-cards/H11.svg';
+import CARD_12_H from './SVG-cards/H12.svg';
+import CARD_13_H from './SVG-cards/H13.svg';
 
-import { ReactComponent as Joker } from './SVG-cards/JOKER.svg';
+import CARD_01_S from './SVG-cards/S01.svg';
+import CARD_02_S from './SVG-cards/S02.svg';
+import CARD_03_S from './SVG-cards/S03.svg';
+import CARD_04_S from './SVG-cards/S04.svg';
+import CARD_05_S from './SVG-cards/S05.svg';
+import CARD_06_S from './SVG-cards/S06.svg';
+import CARD_07_S from './SVG-cards/S07.svg';
+import CARD_08_S from './SVG-cards/S08.svg';
+import CARD_09_S from './SVG-cards/S09.svg';
+import CARD_10_S from './SVG-cards/S10.svg';
+import CARD_11_S from './SVG-cards/S11.svg';
+import CARD_12_S from './SVG-cards/S12.svg';
+import CARD_13_S from './SVG-cards/S13.svg';
+
+// import Joker from './SVG-cards/JOKER.svg';
 
 
 
 class Game extends Component {
   constructor() {
     super();
-    const DECK = ["C01", "D01", "H01", "S01",
-                  "C02", "D02", "H02", "S02",
-                  "C03", "D03", "H03", "S03",
-                  "C04", "D04", "H04", "S04",
-                  "C05", "D05", "H05", "S05",
-                  "C06", "D06", "H06", "S06",
-                  "C07", "D07", "H07", "S07",
-                  "C08", "D08", "H08", "S08",
-                  "C09", "D09", "H09", "S09",
-                  "C10", "D10", "H10", "S10",
-                  "C11", "D11", "H11", "S11",
-                  "C12", "D12", "H12", "S12",
-                  "C13", "D13", "H13", "S13"]
-    var shuffledDECK, player1Hand, player2Hand
+    const DECK = [{name: 'CARD_01_C', svg: CARD_01_C}, {name: 'CARD_01_D', svg: CARD_01_D}, {name: 'CARD_01_H', svg: CARD_01_H}, {name: 'CARD_01_S', svg: CARD_01_S},
+                  {name: 'CARD_02_C', svg: CARD_02_C}, {name: 'CARD_02_D', svg: CARD_02_D}, {name: 'CARD_02_H', svg: CARD_02_H}, {name: 'CARD_02_S', svg: CARD_02_S},
+                  {name: 'CARD_03_C', svg: CARD_03_C}, {name: 'CARD_03_D', svg: CARD_03_D}, {name: 'CARD_03_H', svg: CARD_03_H}, {name: 'CARD_03_S', svg: CARD_03_S},
+                  {name: 'CARD_04_C', svg: CARD_04_C}, {name: 'CARD_04_D', svg: CARD_04_D}, {name: 'CARD_04_H', svg: CARD_04_H}, {name: 'CARD_04_S', svg: CARD_04_S},
+                  {name: 'CARD_05_C', svg: CARD_05_C}, {name: 'CARD_05_D', svg: CARD_05_D}, {name: 'CARD_05_H', svg: CARD_05_H}, {name: 'CARD_05_S', svg: CARD_05_S},
+                  {name: 'CARD_06_C', svg: CARD_06_C}, {name: 'CARD_06_D', svg: CARD_06_D}, {name: 'CARD_06_H', svg: CARD_06_H}, {name: 'CARD_06_S', svg: CARD_06_S},
+                  {name: 'CARD_07_C', svg: CARD_07_C}, {name: 'CARD_07_D', svg: CARD_07_D}, {name: 'CARD_07_H', svg: CARD_07_H}, {name: 'CARD_07_S', svg: CARD_07_S},
+                  {name: 'CARD_08_C', svg: CARD_08_C}, {name: 'CARD_08_D', svg: CARD_08_D}, {name: 'CARD_08_H', svg: CARD_08_H}, {name: 'CARD_08_S', svg: CARD_08_S},
+                  {name: 'CARD_09_C', svg: CARD_09_C}, {name: 'CARD_09_D', svg: CARD_09_D}, {name: 'CARD_09_H', svg: CARD_09_H}, {name: 'CARD_09_S', svg: CARD_09_S},
+                  {name: 'CARD_10_C', svg: CARD_10_C}, {name: 'CARD_10_D', svg: CARD_10_D}, {name: 'CARD_10_H', svg: CARD_10_H}, {name: 'CARD_10_S', svg: CARD_10_S},
+                  {name: 'CARD_11_C', svg: CARD_11_C}, {name: 'CARD_11_D', svg: CARD_11_D}, {name: 'CARD_11_H', svg: CARD_11_H}, {name: 'CARD_11_S', svg: CARD_11_S},
+                  {name: 'CARD_12_C', svg: CARD_12_C}, {name: 'CARD_12_D', svg: CARD_12_D}, {name: 'CARD_12_H', svg: CARD_12_H}, {name: 'CARD_12_S', svg: CARD_12_S},
+                  {name: 'CARD_13_C', svg: CARD_13_C}, {name: 'CARD_13_D', svg: CARD_13_D}, {name: 'CARD_13_H', svg: CARD_13_H}, {name: 'CARD_13_S', svg: CARD_13_S}]
+    var shuffledDECK, player1Hand, player2Hand;
     shuffledDECK = this.shuffle(DECK)
     player1Hand = shuffledDECK.slice(0, 26)
     player2Hand = shuffledDECK.slice(26, 52)
@@ -106,83 +107,28 @@ class Game extends Component {
   render() {
     const sortHand = (player, hand) => {
       if (player === 1) {
-        this.setState({player1Hand: hand.sort((a, b) => a > b ? 1 : -1)})
+        debugger
+        this.setState({player1Hand: hand.sort((a, b) => a.name > b.name ? 1 : -1)})
       } else if (player === 2) {
-        this.setState({player2Hand: hand.sort((a, b) => a > b ? 1 : -1)})
+        this.setState({player2Hand: hand.sort((a, b) => a.name > b.name ? 1 : -1)})
       }
     }
     return (
       <div className='container'>
-        <C01></C01>
-        <C02></C02>
-        <C03></C03>
-        <C04></C04>
-        <C05></C05>
-        <C06></C06>
-        <C07></C07>
-        <C08></C08>
-        <C09></C09>
-        <C10></C10>
-        <C11></C11>
-        <C12></C12>
-        <C13></C13>
-
-        <D01></D01>
-        <D02></D02>
-        <D03></D03>
-        <D04></D04>
-        <D05></D05>
-        <D06></D06>
-        <D07></D07>
-        <D08></D08>
-        <D09></D09>
-        <D10></D10>
-        <D11></D11>
-        <D12></D12>
-        <D13></D13>
-
-        <H01></H01>
-        <H02></H02>
-        <H03></H03>
-        <H04></H04>
-        <H05></H05>
-        <H06></H06>
-        <H07></H07>
-        <H08></H08>
-        <H09></H09>
-        <H10></H10>
-        <H11></H11>
-        <H12></H12>
-        <H13></H13>
-
-        <S01></S01>
-        <S02></S02>
-        <S03></S03>
-        <S04></S04>
-        <S05></S05>
-        <S06></S06>
-        <S07></S07>
-        <S08></S08>
-        <S09></S09>
-        <S10></S10>
-        <S11></S11>
-        <S12></S12>
-        <S13></S13>
-
-        <Joker></Joker>
-
         <h1>GAME</h1>
-        <h2>shuffled deck: {this.state.shuffledDECK.join(', ')}</h2>
         <br></br>
-        <h2>player 1 hand: {this.state.player1Hand.join(', ')}</h2>
         <h2>player 1 hand: </h2>
+        <ReactSVG src='./SVG-cards/D01.svg' />
         {this.state.player1Hand.map((card, index) =>
-          eval(`1 + 1`)
+          <img src={card.svg} />
         )}
         <div className='btn btn-dark' onClick={() => sortHand(1, this.state.player1Hand)}>
           Ordenar mão
         </div>
-        <h2>player 2 hand: {this.state.player2Hand.join(', ')}</h2>
+        <h2>player 2 hand: </h2>
+        {this.state.player2Hand.map((card, index) =>
+          <img src={card.svg} />
+        )}
         <div className='btn btn-dark' onClick={() => sortHand(2, this.state.player2Hand)}>
           Ordenar mão
         </div>
